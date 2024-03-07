9b9de032ad07ee1707e69649b2c00db214450826
anapaunavas <anapaulahong@gmail.com>
2024-03-07 15:09:27 -0600

    get and create posts

--- api_docker/db.js
+++ api_docker/db.js
@@ -22,6 +22,15 @@ export async function createBlog(title, content) {
     }
 }
 
+export async function getAllPosts() {
+    const [rows] = await conn.query('SELECT * FROM blog_posts')
+    return rows
+}
+
+export async function createPost(title, content) {
+    const [result] = await db.query('INSERT INTO blog_posts (title, content) VALUES (?, ?)', [title, content])
+    return result
+}
 
 export async function deleteBlog(id) {
     try {
